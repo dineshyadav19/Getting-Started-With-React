f396f9201d55e9f093c13570d057b785247ab120
dineshyadav19 <dinesh.2108.yadav@gmail.com>
2021-08-15 09:46:16 +0530

    Project-3 Accordion Toggle App

---
+++ 04-accordion/src/Accordion.js
@@ -0,0 +1,19 @@
+import React, { useState } from 'react'
+import { AiOutlineMinus, AiOutlinePlus } from 'react-icons/ai';
+
+const Accordion = ({title, info}) => {
+    const [answer, setAnswer] = useState(false)
+    return (
+        <article className='question'>
+            <header>
+                <h4>{title}</h4>
+                <button className='btn' onClick={() => setAnswer(!answer)}>
+                    {answer ? <AiOutlineMinus /> : <AiOutlinePlus />}
+                </button>
+            </header>
+            {answer && <p>{info}</p>}
+        </article>
+    )
+}
+
+export default Accordion;
